d1771636f7d637cfa9da5d891e8b9194fe9f3dc2
LunaMoon05 <mihailstepanov.stepanoff2015@yandex.ru>
2021-08-24 19:29:55 +0300

    сверстан футер + обычное поле ввода

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,6 @@ import '../assets/fonts/fonts'
 import '../scss/index.scss'
 // Компоненты
 import '../ui-kit/header/header'
+import '../ui-kit/footer/footer'
 import '../ui-kit/button/button'
+import '../ui-kit/field/field'
